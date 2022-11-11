d3b6c0c0633669c035ec9288d25ce5d6bf836c03
Al Amin <alamin931761@gmail.com>
2022-11-11 19:48:48 +0600

    Coupon

--- js/app.js
+++ js/app.js
@@ -25,3 +25,27 @@ function updatePrice(itemId, ItemPrice) {
     const totalPrice = document.getElementById('total-price');
     totalPrice.innerText = parseFloat(bestPrice) + parseFloat(memoryCost) + parseFloat(storageCost) + parseFloat(deliveryCost);
 }
+// Promo message 
+document.getElementById('success').style.display = 'none';
+document.getElementById('error').style.display = 'none';
+
+// Promo code 
+const fakeCode = "iMac"
+document.getElementById('apply-button').addEventListener('click', function () {
+    const code = document.getElementById('promo-input').value;
+    if (code === fakeCode) {
+        const totalPrice = document.getElementById('total-price');
+        const updatePrice = parseFloat(totalPrice.innerText) - (parseFloat(totalPrice.innerText) * 0.2);
+        totalPrice.innerText = updatePrice;
+        // clear input field 
+        document.getElementById('promo-input').value = ''
+        // message 
+        document.getElementById('success').style.display = 'block'
+    }
+    else {
+        // clear input field
+        document.getElementById('promo-input').value = ''
+        // message 
+        document.getElementById('error').style.display = 'block';
+    }
+});
