208e892437d8ff3fce3300907f94ede6920c9c10
Al Amin <alamin931761@gmail.com>
2022-11-11 18:14:33 +0600

    Short code using functions

--- js/app.js
+++ js/app.js
@@ -1,38 +1,16 @@
-// 8 GB Memory 
-document.getElementById('eightGB').addEventListener('click', function () {
-    updatePrice('memory-cost', 0);
-});
+function onClick(clickId, updateId, price) {
+    document.getElementById(clickId).addEventListener('click', function () {
+        updatePrice(updateId, price);
+    });
+}
 
-// 16 GB Memory 
-document.getElementById('sixteenGB').addEventListener('click', function () {
-    updatePrice('memory-cost', 200);
-});
-
-// 256 GB SSD 
-document.getElementById('ssd1').addEventListener('click', function () {
-    updatePrice('storage-cost', 0);
-});
-
-// 512 GB SSD
-document.getElementById('ssd2').addEventListener('click', function () {
-    updatePrice('storage-cost', 400);
-});
-
-// 1 TB SSD 
-document.getElementById('ssd3').addEventListener('click', function () {
-    updatePrice('storage-cost', 800);
-});
-
-// Free Delivery
-document.getElementById('free-delivery').addEventListener('click', function () {
-    updatePrice('delivery-cost', 0);
-});
-
-// Paid Delivery
-document.getElementById('paid-delivery').addEventListener('click', function () {
-    updatePrice('delivery-cost', 21);
-});
-
+onClick('eightGB', 'memory-cost', 0);
+onClick('sixteenGB', 'memory-cost', 200);
+onClick('ssd1', 'storage-cost', 0);
+onClick('ssd2', 'storage-cost', 400);
+onClick('ssd3', 'storage-cost', 800);
+onClick('free-delivery', 'delivery-cost', 0);
+onClick('paid-delivery', 'delivery-cost', 21);
 
 function updatePrice(itemId, ItemPrice) {
     const item = document.getElementById(itemId);
